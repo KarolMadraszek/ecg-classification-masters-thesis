226089c9e7ef484d410eac614cd8246f7259b34f
KarolMadraszek <karol.madraszek.03@gmail.com>
2026-09-15 13:25:18 +0200

    Poprawki w pliku generoatora danych

--- src/generator.py
+++ src/generator.py
@@ -3,29 +3,19 @@ import numpy as np
 import tensorflow as tf
 
 def create_tukey_window(length, alpha=0.1):
-    if alpha <= 0:
-        return np.ones(length)
-    elif alpha >= 1:
-        return np.hanning(length)
-
-    x = np.linspace(0, 1, length)
-    w = np.ones(length)
-
-    # Lewy brzeg
-    first_condition = x < alpha / 2
-    w[first_condition] = 0.5 * (1 + np.cos(2 * np.pi / alpha * (x[first_condition] - alpha / 2)))
-
-    # Prawy brzeg
-    third_condition = x >= 1 - alpha / 2
-    w[third_condition] = 0.5 * (1 + np.cos(2 * np.pi / alpha * (x[third_condition] - 1 + alpha / 2)))
-
+    n = np.arange(length)
+    width = int(alpha * (length - 1) / 2.0)
+    w = np.ones(length, dtype=np.float32)
+    if width > 0:
+        w[:width] = 0.5 * (1.0 + np.cos(np.pi * (-1.0 + 2.0 * n[:width] / (2.0 * width))))
+        w[-width:] = 0.5 * (1.0 + np.cos(np.pi * (-2.0 + 2.0 * (length - 1 - n[-width:]) / (2.0 * width))))
     return w
 
 class H5MemorySafeGenerator(tf.keras.utils.Sequence):
     def __init__(self, h5_path, split, batch_size=32, shuffle=False,
                  crop=False, crop_range=(50, 950),
                  taper_edges=False, freq_masking=False):
-        self.h5_path = h5_path
+        self.h5_path = str(h5_path)
         self.split = split
         self.batch_size = batch_size
         self.shuffle = shuffle
@@ -34,8 +24,8 @@ class H5MemorySafeGenerator(tf.keras.utils.Sequence):
         self.taper_edges = taper_edges
         self.freq_masking = freq_masking
 
-        with h5py.File(self.h5_path, 'r') as f:
-            self.num_samples = f[self.split]['X'].shape[0]
+        self.f = h5py.File(self.h5_path, 'r')
+        self.num_samples = self.f[self.split]['X'].shape[0]
 
         self.indices = np.arange(self.num_samples)
         if self.shuffle:
@@ -44,19 +34,23 @@ class H5MemorySafeGenerator(tf.keras.utils.Sequence):
         self.tukey_window = None
         if self.taper_edges:
             length = self.crop_range[1] - self.crop_range[0] if self.crop else 1000
-            # (1, 1, czas, 1)
             self.tukey_window = create_tukey_window(length, alpha=0.1).reshape(1, 1, -1, 1)
 
     def __len__(self):
         return int(np.ceil(self.num_samples / float(self.batch_size)))
 
     def __getitem__(self, idx):
-        batch_indices = self.indices[idx * self.batch_size: (idx + 1) * self.batch_size]
-        batch_indices = np.sort(batch_indices)
+        batch_indices = self.indices[idx * self.batch_size : (idx + 1) * self.batch_size]
+        sorted_order = np.argsort(batch_indices)
+        sorted_indices = batch_indices[sorted_order]
 
-        with h5py.File(self.h5_path, 'r') as f:
-            X_batch = np.array(f[self.split]['X'][batch_indices], dtype=np.float32)
-            y_batch = np.array(f[self.split]['y'][batch_indices], dtype=np.float32)
+        X_batch = np.array(self.f[self.split]['X'][sorted_indices], dtype=np.float32)
+        y_batch = np.array(self.f[self.split]['y'][sorted_indices], dtype=np.float32)
+
+        inverse_order = np.empty_like(sorted_order)
+        inverse_order[sorted_order] = np.arange(len(sorted_order))
+        X_batch = X_batch[inverse_order]
+        y_batch = y_batch[inverse_order]
 
         if self.crop:
             start, end = self.crop_range
@@ -75,12 +69,17 @@ class H5MemorySafeGenerator(tf.keras.utils.Sequence):
             np.random.shuffle(self.indices)
 
     def _apply_freq_mask(self, X_batch, max_mask_pct=0.15):
-        # Założenie kształtu wejścia: (batch, freq, time, channels)
         n_freq = X_batch.shape[1]
         for i in range(X_batch.shape[0]):
             if np.random.rand() < 0.7:
                 mask_size = np.random.randint(1, int(n_freq * max_mask_pct) + 1)
                 mask_start = np.random.randint(0, n_freq - mask_size)
                 X_batch[i, mask_start: mask_start + mask_size, :, :] = 0.0
-
         return X_batch
+
+    def __del__(self):
+        if hasattr(self, 'f') and self.f:
+            try:
+                self.f.close()
+            except Exception:
+                pass
