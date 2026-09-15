28efc7fc8743a988b85d9de16c1ecf6dcad41384
KarolMadraszek <karol.madraszek.03@gmail.com>
2026-09-15 13:32:53 +0200

    Poprawki optymalizacyjne w pliku generatora danych (jednorazowe ładowanie danych do pamięci RAM)

--- src/generator.py
+++ src/generator.py
@@ -1,6 +1,7 @@
 import h5py
 import numpy as np
 import tensorflow as tf
+from tqdm import tqdm
 
 def create_tukey_window(length, alpha=0.1):
     n = np.arange(length)
@@ -11,11 +12,11 @@ def create_tukey_window(length, alpha=0.1):
         w[-width:] = 0.5 * (1.0 + np.cos(np.pi * (-2.0 + 2.0 * (length - 1 - n[-width:]) / (2.0 * width))))
     return w
 
+
 class H5MemorySafeGenerator(tf.keras.utils.Sequence):
     def __init__(self, h5_path, split, batch_size=32, shuffle=False,
                  crop=False, crop_range=(50, 950),
                  taper_edges=False, freq_masking=False):
-        self.h5_path = str(h5_path)
         self.split = split
         self.batch_size = batch_size
         self.shuffle = shuffle
@@ -24,9 +25,20 @@ class H5MemorySafeGenerator(tf.keras.utils.Sequence):
         self.taper_edges = taper_edges
         self.freq_masking = freq_masking
 
-        self.f = h5py.File(self.h5_path, 'r')
-        self.num_samples = self.f[self.split]['X'].shape[0]
+        with h5py.File(str(h5_path), 'r') as f:
+            total_samples = f[self.split]['X'].shape[0]
+            orig_shape = f[self.split]['X'].shape
 
+            print(f"[{split.upper()}] Alokacja bufora RAM dla {total_samples} próbek...")
+            self.X_data = np.empty(orig_shape, dtype=np.float32)
+            self.y_data = np.array(f[self.split]['y'], dtype=np.float32)
+
+            chunk_size = 512
+            for i in tqdm(range(0, total_samples, chunk_size), desc=f"Wczytywanie {split.upper()} do RAM"):
+                end = min(i + chunk_size, total_samples)
+                self.X_data[i:end] = f[self.split]['X'][i:end]
+
+        self.num_samples = total_samples
         self.indices = np.arange(self.num_samples)
         if self.shuffle:
             np.random.shuffle(self.indices)
@@ -41,16 +53,9 @@ class H5MemorySafeGenerator(tf.keras.utils.Sequence):
 
     def __getitem__(self, idx):
         batch_indices = self.indices[idx * self.batch_size : (idx + 1) * self.batch_size]
-        sorted_order = np.argsort(batch_indices)
-        sorted_indices = batch_indices[sorted_order]
 
-        X_batch = np.array(self.f[self.split]['X'][sorted_indices], dtype=np.float32)
-        y_batch = np.array(self.f[self.split]['y'][sorted_indices], dtype=np.float32)
-
-        inverse_order = np.empty_like(sorted_order)
-        inverse_order[sorted_order] = np.arange(len(sorted_order))
-        X_batch = X_batch[inverse_order]
-        y_batch = y_batch[inverse_order]
+        X_batch = self.X_data[batch_indices].copy()
+        y_batch = self.y_data[batch_indices]
 
         if self.crop:
             start, end = self.crop_range
@@ -76,10 +81,3 @@ class H5MemorySafeGenerator(tf.keras.utils.Sequence):
                 mask_start = np.random.randint(0, n_freq - mask_size)
                 X_batch[i, mask_start: mask_start + mask_size, :, :] = 0.0
         return X_batch
-
-    def __del__(self):
-        if hasattr(self, 'f') and self.f:
-            try:
-                self.f.close()
-            except Exception:
-                pass
